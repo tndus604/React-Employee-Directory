fd085236591166b0079e9562cf31383ebaca39fd
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-20 17:39:47 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Use the search box to narrow your results!</p>
+        </div>
+    )
+}
+
+export default Header;
